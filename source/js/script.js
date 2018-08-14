af81a4635ded1793e861fbe4f247124bc397ccad
Сергей <agrael@yandex.ru>
2018-08-14 11:56:24 +0300

    Добавление github pages

--- source/js/script.js
+++ source/js/script.js
@@ -1,25 +1,24 @@
 const URL = "https://api.jsonbin.io/b/5b683d097b212953678c03dd";
 const configList = document.querySelector(".main-content__configs");
-const cardTemplate = document.querySelector("#config-template")
-    .content
-    .querySelector(".card");
+const cardTemplate = document.querySelector("#config-template").querySelector(".card");
 const loader = configList.querySelector(".config__loader");
-const networkError = document.querySelector("#config-template")
-    .content
-    .querySelector(".config__error");
-
+const networkError = document.querySelector("#config-template").querySelector(".config__error");
 
 window.load = function() {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = "json";
+    var xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", function () {
-        if (xhr.status === 200) {
-            onSuccess(xhr.response);
-            removeLoader(loader);
-        } else {
-            onError(networkError);
-            removeLoader(loader);
+    xhr.addEventListener('load', function () {
+        try {
+            var massive = JSON.parse(xhr.responseText);
+            if (xhr.status === 200) {
+                onSuccess(massive);
+                removeLoader(loader);
+            } else {
+                onError(networkError);
+                removeLoader(loader);
+            }
+        } catch (err) {
+            console.error(err.message);
         }
     });
 
@@ -49,4 +48,3 @@ const removeLoader = function (loaderIcon) {
 };
 
 window.load();
-
